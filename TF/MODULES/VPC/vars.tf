d19d896401644d1d6c21e828b6099495ca806b93
Michael Olukosi <mich.olukosi@gmail.com>
2023-09-11 11:50:53 -0400

    Adding VPC Module

---
+++ TF/MODULES/VPC/vars.tf
@@ -0,0 +1,8 @@
+variable "required_tags"{}
+variable "vpc_cidr_block"{}
+variable "availability_zones"{}
+variable "public_subnets"{}
+variable "private_subnets"{}
+variable "database_subnets"{}
+variable "java_app_subnets"{}
+variable "oracle_db_subnets"{}
