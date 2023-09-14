e2b7444ded3518712cf41c913a9c922c0d4104a4
Michael Olukosi <mich.olukosi@gmail.com>
2023-09-14 01:57:54 -0400

    removed access keys

--- STACK-EC2-TF/provider.tf
+++ STACK-EC2-TF/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = var.AWS_REGION
-  access_key = locals.db_creds.AWS_ACCESS_KEY
-  secret_key = locals.db_creds.AWS_SECRET_KEY
+  # access_key = locals.db_creds.AWS_ACCESS_KEY
+  # secret_key = locals.db_creds.AWS_SECRET_KEY
 
   assume_role {
     role_arn = "arn:aws:iam::541478845275:role/Engineer"
