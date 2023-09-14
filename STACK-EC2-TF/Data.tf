ba7f005c6d35c7af50f9d1bd0a38020ac0ad9962
Michael Olukosi <mich.olukosi@gmail.com>
2023-09-14 00:24:57 -0400

    modified code7

--- STACK-EC2-TF/Data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_secretsmanager_secret" "stack_clixx_secrets" {
-  name = "stack_clixx_secrets"
-}
-
--- STACK-EC2-TF/configs.tf
+++ STACK-EC2-TF/configs.tf
@@ -5,11 +5,11 @@ data "template_file" "bootstrap" {
     GIT_REPO          = "https://github.com/stackitgit/CliXX_Retail_Repository.git",
     STACK_CLIXX_EFS   = aws_efs_file_system.stack-clixx-EFS.id,
     LOAD_BALANCER_DNS = aws_lb.clixx-app-load-balancer.dns_name,
-    DATABASE          = data.aws_secretsmanager_secret.stack_clixx_secret["DB_NAME"],
-    USER              = data.aws_secretsmanager_secret.stack_clixx_secret["DB_USER"],
-    PASSWORD          = data.aws_secretsmanager_secret.stack_clixx_secret["DB_PASSWORD"],
+    DATABASE          = locals.db_creds.DB_NAME,
+    USER              = locals.db_creds.DB_USER,
+    PASSWORD          = locals.db_creds.DB_PASSWORD,
     RDS_ENDPOINT      = split(":", aws_db_instance.clixx-retail-app.endpoint)[0],
-    ORIGINAL_ENDPOINT = data.aws_secretsmanager_secret.stack_clixx_secret["OLD_ENDPOINT"]
+    ORIGINAL_ENDPOINT = locals.db_creds.OLD_ENDPOINT
   }
 }
 
